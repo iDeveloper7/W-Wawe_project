23ed80f506d6144dafef353bb801c33aed41dd45
Карсаков Владимир Викторович <VKarsakov@borlas.ru>
2022-09-05 17:59:57 +0300

    working select and accordion

--- index.js
+++ index.js
@@ -7,3 +7,62 @@ const choices = new Choices(element, {
   itemSelectText: '',
 
 });
+
+//custom accordion
+new Accordion('.guests__accordion', {
+  elementClass: 'accordion__item',
+  triggerClass: 'accordion__btn',
+  panelClass: 'accordion__panel-list',
+  activeClass: 'accordion__panel',
+});
+
+let accordionBtn = document.querySelectorAll('.accordion__btn');
+let accordionEllipses = document.querySelectorAll('.accordion__btn-img');
+let accordionPanelList = document.querySelectorAll('.accordion__panel-list');
+
+accordionBtn.forEach(function (btn) {
+  btn.addEventListener('click', function (data) {
+    const btnPath = data.currentTarget.dataset.btnpath;
+    console.log(btnPath);
+
+    accordionEllipses.forEach(function (el) {
+      el.classList.remove('accordion__btn-img_active');
+    })
+
+    accordionPanelList.forEach(function (el) {
+      el.classList.remove('accordion__panel-list_active');
+    })
+
+
+    if (document.querySelector('[aria-expanded = "true"]')) {
+      document.querySelector(`[data-btntarget = "${btnPath}"]`).classList.add('accordion__btn-img_active');
+      document.querySelector(`[data-ultarget = "${btnPath}"]`).classList.add('accordion__panel-list_active');
+    } else {
+      accordionEllipses.forEach(function (ell) {
+        ell.classList.remove('accordion__btn-img_active');
+      })
+    }
+  })
+})
+
+// tabs
+
+const tabsBtn = document.querySelectorAll('.accordion__panel-btn');
+const tabsItem = document.querySelectorAll('.guests__person');
+
+tabsBtn.forEach(function (el) {
+  el.addEventListener('click', function (data) {
+    const path = data.currentTarget.dataset.path;
+    console.log(path);
+
+    tabsBtn.forEach(function (btn) {
+      btn.classList.remove('accordion__panel-btn_active');
+    })
+    data.currentTarget.classList.add('accordion__panel-btn_active');
+
+    tabsItem.forEach(function (item) {
+      item.classList.remove('guests__person_active');
+    })
+    document.querySelector(`[data-target = "${path}"]`).classList.add('guests__person_active');
+  })
+})
